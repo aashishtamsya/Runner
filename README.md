7b5a64506be14c058d15096c8343b12211df4cfa
Aashish Tamsya <aashish.tamsya@gmail.com>
2017-05-17 09:23:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Runner - iOS App
+
+iOS Application to motivate people who want to begin running.
+
---
+++ Runner/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Runner
+//
+//  Created by Aashish Tamsya on 17/05/17.
+//  Copyright © 2017 Aashish Tamsya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
